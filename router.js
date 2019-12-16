a82df9783f669e954ce0deaf62031ec60fa76257
ocph23@gmail.com <ocph23@gmail.com>
2019-12-16 13:51:57 +0900

    publish

--- router.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router	= express.Router();
-
-router.get('/', function (req, res) {
-	var homePage = "This is the default home page";
-	res.status(200).send(homePage);
-});
-
-module.exports = router;
