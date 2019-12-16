cdf5325404e7fb1c0f18cc1013c6aeadc7478c98
ocph23@gmail.com <ocph23@gmail.com>
2019-12-16 12:45:55 +0900

    remove serber

--- server/models/IAuthenticateService.cs
+++
@@ -1,7 +0,0 @@
- public interface IAuthenticateService {
-     bool IsAuthenticated (TokenRequest request, out string token);
- }
-
- public interface IUserManagementService {
-     bool IsValidUser (string username, string password);
- }
